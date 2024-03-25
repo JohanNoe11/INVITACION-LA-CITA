6f7c2b1663d1c97cf9101aca3f8eaf29c9151831
JohanNoe11 <164843646+JohanNoe11@users.noreply.github.com>
2024-03-25 00:55:17 -0500

    Invitando a una salida con Sofia jsjsjjs

---
+++ INVITACIÓN PARA LA CITA - copia/script.js
@@ -0,0 +1,27 @@
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Estas segura?',
+  'Piensalo bien',
+  'Piensalo muy bien',
+  'Solo piensalo',
+  'Mira el otro botón',
+  'Ya tomaste la decisión',
+  'Deja la bobada sofia y elige bien'
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
